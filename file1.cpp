4a0008958459426f4590f3b58e7088d52cf1849c
maulik <maulikchhabra@gmail.com>
2019-03-18 13:05:05 +0530

    Q3 Changed file 1 and 2 (c2)

--- file1.cpp
+++ file1.cpp
@@ -2,6 +2,10 @@
 using namespace std;
 int main()
 {
+	int x;
+	cout<<"Enter the value:";
+	cin>>x;
 	cout<<"Hello";
 	cout<<"Its a good day";
-	
+	cout<<"Your number is: "<<x<<endl;
+}	
--- file2.cpp
+++ file2.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main()
 {
 	cout<<"SCM mid sem question 3";
-	
+	cout<<"Github"`
 }
 

cd4f02822ce67b170877b5c67d499bcdedf2fd8b
maulik <maulikchhabra@gmail.com>
2019-03-18 12:43:55 +0530

    Q2 calculator with addition and substraction function

--- file.cpp
+++ file.cpp
@@ -10,15 +10,30 @@ class calc
 			sum=x+y;
 			return sum;
 		}
-		
+		int substract()
+		{
+			int z;
+			z=x-y;
+			return z;
+		}
 };
 int main()
 {
 	calc obj;
 	cout<<"Enter two digits to perform binary operations:";
 	cin>>obj.a>>obj.b;	
-	int sum;
-	sum=obj.add();
-	cout<<"sum:"<<sum<<endl;
+	int sum,difference;
+	int choice;
+	cout<<"Enter your choice: 1.Addition 2.Substraction";
+	cin>>choice;
+	switch(choice)
+	{
+		case 1: sum=obj.add();
+                        cout<<"sum:"<<sum<<endl;
+                        break;  	
+	        case 2:difference=obj.substract();
+		       cout<<"Difference is"<<difference<<endl;
+		       break;
+	}
 	return 0;
 }

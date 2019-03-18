59f1b661cf2e679cab75550cde56d3965d3a7c0d
maulik <maulikchhabra@gmail.com>
2019-03-18 12:23:16 +0530

    Q1 .gitignore

---
+++ f1.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	public:
+		void func()
+		{
+			cout<<"Hello!";
+			cout<<"SCM Midsem ";
+		}
+};
+int main()
+{
+	 A obj1;
+	 obj1.func();
+	 return 0;
+}
+
